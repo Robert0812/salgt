0556fe0d58a9dcdafcd172b377bbd9f36dd8bd25
Rui Zhao <fantasticzr@gmail.com>
2014-05-28 22:18:32 +0000

    add salience groundtruth showing

--- saltrain.py
+++ saltrain.py
@@ -5,7 +5,7 @@ import theano
 import theano.tensor as T 
 
 from sals.models import sgd_optimizer
-from sals.models import FCLayer, GeneralModel, ConvLayer
+from sals.models import FCLayer, GeneralModel, ConvLayer, ConvPoolLayer
 from sals.utils.DataHelper import DataMan_viper
 from sals.utils.FunctionHelper import *
 from sals.utils.utils import savefile, loadfile
@@ -17,7 +17,7 @@ def main():
 		datapath = '../data_viper/'
 	else:
 		datapath = '../data/'
-		
+
 	# prepare training data for supervised salience training 
 	datafile_viper = datapath + 'viper_class.pkl'
 	if not os.path.isfile(datafile_viper):
@@ -29,7 +29,7 @@ def main():
 
 	bs = 100
 	imL = 48
-	nfilter1 = 32
+	nfilter1 = 16
 	filterL = 5
 	recfield = 2
 
@@ -37,24 +37,25 @@ def main():
 	y = T.vector(name='y', dtype=theano.config.floatX)
 
 	layer0 = x.reshape((bs, 3, imL, imL))
-	conv1 = ConvLayer(input=layer0, image_shape=(bs, 3, imL, imL), 
+	conv1 = ConvPoolLayer(input=layer0, image_shape=(bs, 3, imL, imL), 
 				filter_shape=(nfilter1, 3, filterL, filterL), 
-				flatten=True,
+				pool_shape = (recfield, recfield), 
+				flatten=False,
 				actfun=tanh,
-				tag='_conv1')
+				tag='_convpool1')
 
 	# outL = np.floor((imL-filterL+1.)/recfield).astype(np.int)
-	outL = imL-filterL+1
+	outL = np.floor((imL-filterL+1.)/recfield).astype(np.int)
 
-	# nfilter2 = 16
-	# filterL2 = 3
-	# conv2 = ConvLayer(input=conv1.output(), image_shape=(bs, nfilter1, outL, outL), 
-	# 			filter_shape=(nfilter2, nfilter1, filterL2, filterL2), 
-	# 			flatten=False,
-	# 			actfun=tanh,
-	# 			tag='_conv2')
+	nfilter2 = 16
+	filterL2 = 3
+	conv2 = ConvLayer(input=conv1.output(), image_shape=(bs, nfilter1, outL, outL), 
+				filter_shape=(nfilter2, nfilter1, filterL2, filterL2), 
+				flatten=True,
+				actfun=tanh,
+				tag='_conv2')
 
-	# outL2 = outL-filterL2+1
+	outL2 = outL-filterL2+1
 
 	# nfilter3 = 16
 	# filterL3 = 3
@@ -66,8 +67,8 @@ def main():
 
 	# outL3 = outL2-filterL3+1
 
-	fc3 = FCLayer(input=conv1.output(), n_in=nfilter1*outL*outL, n_out=2048, actfun=sigmoid, tag='_fc3')
-	fc4 = FCLayer(input=fc3.output(), n_in=2048, n_out=256, actfun=sigmoid, tag='_fc4')
+	fc3 = FCLayer(input=conv2.output(), n_in=nfilter2*outL2*outL2, n_out=1024, actfun=sigmoid, tag='_fc3')
+	fc4 = FCLayer(input=fc3.output(), n_in=1024, n_out=256, actfun=sigmoid, tag='_fc4')
 	fc5 = FCLayer(input=fc4.output(), n_in=256, n_out=1, actfun=sigmoid, tag='_fc5')
 	params_cmb = conv1.params + fc3.params + fc4.params + fc5.params
 
@@ -83,7 +84,7 @@ def main():
 	sgd = sgd_optimizer(data=viper,  
 					model=model,
 					batch_size=bs, 
-					learning_rate=0.001,
+					learning_rate=0.0001,
 					learning_rate_decay=0.7,
 					n_epochs=20)
 
