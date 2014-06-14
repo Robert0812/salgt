aba76eb4b345b4ce2baea90f1e9edac53a69823b
Rui Zhao <fantasticzr@gmail.com>
2014-06-14 21:24:13 +0000

    save patch matching maps

--- reid/evalreid.py
+++ reid/evalreid.py
@@ -1,9 +1,12 @@
+import os
+import numpy as np
+
 from glob import glob
 from scipy.spatial.distance import cdist	
 import time
 
 from sals.utils.utils import *
-import numpy as np
+
 
 def patchmatch(imfeat1, imfeat2, ny, nx):
 	''' generate patch matching distances, return an ny by nx distance matrix '''
@@ -16,6 +19,8 @@ def patchmatch(imfeat1, imfeat2, ny, nx):
 		pwdist[y] = dist.min(axis=1)
 		indmat[y] = dist.argmin(axis=1)
 
+	return pwdist, indmat
+
 
 def rearrange_feat(imfeats, ny, nx):
 	'''
@@ -26,8 +31,17 @@ def rearrange_feat(imfeats, ny, nx):
 	return imfeats.reshape((n_im, ny, nx, n_feat), order='F')
 
 def compute_cmc(pwdist):
-	''' compute CMC '''
+	''' compute CMC 
+		INPUT: pairwise matching distance (query to gallery distances)
+		OUTPUT: a vector representing CMC 
+	'''
 	
+	qsize, gsize = pwdists.shape
+	
+	order = np.argsort(pwdist)
+	match = order == np.tile(np.arange(qsize).reshape((qsize, 1)), (1, gsize))
+	cmc = match.sum(axis=0).cumsum()/float(qsize)
+	return cmc
 	
 def main():
 
@@ -55,22 +69,37 @@ def main():
 		
 		n_query = feats_query.shape[0]
 		n_gallery = feats_gallery.shape[0]
-		pwdists = np.zeros((n_query, n_gallery))
+		pwdists = np.zeros((n_query, n_gallery, ny, nx), dtype=float)
+		indmats = np.zeros((n_query, n_gallery, ny, nx), dtype=int)
 		for iq in range(n_query):
 			for ig in range(n_gallery):
 				t0 = time.clock()
-				pwdists[iq, ig] = patchmatch(feats_query[iq], feats_gallery[ig], ny, nx)
+				pwdists[iq, ig, :, :], indmats[iq, ig, :, :] = patchmatch(feats_query[iq], feats_gallery[ig], ny, nx)
 				t1 = time.clock()
 				print '{0:02d}-{1:02d}: eclapsed {2:.2f} sec'.format(iq, ig, t1-t0)
 
 		savefile(patch_match_path, pwdists)
 	
 	else:
-		loadfile(patch_match_path)
+		pwdists = loadfile(patch_match_path)
 
+	# only patch matching
+	sigma = 2.8
+	n_query = feats_query.shape[0]
+	n_gallery = feats_gallery.shape[0]
+	pwdists_pm = np.zeros((n_query, n_gallery))
+	for iq in range(n_query):
+		for ig in range(n_gallery):
+			pwdists_pm[iq, ig] = np.exp(-(pwdists[iq, ig]**2)/(sigma**2)).sum()
+
+	cmc = compute_cmc(pwdists_pm)
+	import pylab as pl
+
+	pl.plot(cmc)
+	pl.show()
+	pl.savefig('tmp1.jpg')
+	os.system('open tmp1.jpg')
 	# salience matching 
-
-
 
 
 if __name__ == '__main__':
